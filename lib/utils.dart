08f97a98fa00ec25d6c275a45ffb43194355023c
David Bonelo <bonelodev@gmail.com>
2021-11-04 10:39:25 -0500

    deploy gh page

--- lib/utils.dart
+++
@@ -1,8 +0,0 @@
-/// Returns the number as string without decimals if it ends with '.0', otherwise returns the double as normal.
-String doubleToIntString(double number) {
-  var numString = number.toString();
-  if (numString.endsWith('.0')) {
-    return numString.replaceRange(numString.length - 2, null, '');
-  }
-  return numString;
-}
